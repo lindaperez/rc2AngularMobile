74adcca056888428db3ab2e1c15a3757c75e17b1
Linda Perez <rosmery.p.p@gmail.com>
2019-03-08 13:48:11 -0800

    Second week excercise 6: Sigle Page 2: use of route parameters within the URL to pass information to another component dishdetail/id

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -10,11 +10,14 @@ import  { AboutComponent} from  '../about/about.component';
 import { HomeComponent} from '../home/home.component';
 import { ContactComponent } from '../contact/contact.component';
 
+
 export const routes: Routes = [
   {path: 'home',component: HomeComponent },
   {path: 'menu', component: MenuComponent},
   {path: 'contact', component: ContactComponent},
+  {path: 'dishdetail/:id',component: DishdetailComponent},
   {path:'',redirectTo:'/home', pathMatch:'full'},
+
 
 
  ];
