a7d6c1b7fde3882bf4d21443f44fd2194511776d
Linda Perez <rosmery.p.p@gmail.com>
2019-03-27 16:46:26 -0700

    Fourth Week: Animations from top ease-in ease-out about and menu  app.animation

--- conFusion/src/app/animations/app.animation.ts
+++ conFusion/src/app/animations/app.animation.ts
@@ -14,4 +14,25 @@ return trigger('visibility', [
   ])
 };
 
+export function flyInOut() {
+  return trigger('flyInOut', [
+    state('*', style({ opacity: 1, transform: 'translateX(0)'})),
+    transition(':enter', [
+      style({ transform: 'translateY(-100%)', opacity: 0 }),
+      animate('500ms ease-in')
+    ]),
+    transition(':leave', [
+      animate('500ms ease-out', style({ transform: 'translateY(100%)', opacity: 0}))
+    ])
+  ]);
+}
 
+export function expand() {
+  return trigger('expand', [
+    state('*', style({ opacity: 1, transform: 'translateX(0)' })),
+    transition(':enter', [
+      style({ transform: 'translateY(-50%)', opacity:0 }),
+      animate('200ms ease-in', style({ opacity: 1, transform: 'translateX(0)' }))
+    ])
+  ]);
+}
--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {Dish} from '../share/dish';
 import { DishService } from '../services/dish.service';
-
+import { flyInOut } from '../animations/app.animation';
 
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut()
+  ]
 })
 
 export class MenuComponent implements OnInit {
